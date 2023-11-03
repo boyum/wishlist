0a39bf2843304df6a9f888ef802bcd386408b9e6
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-11-03 14:25:50 +0100

    feat: gooey button

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -10,27 +10,62 @@
   };
 </script>
 
-<button on:click={handleClick} {type}><slot /></button>
+<button on:click={handleClick} {type}>
+  <div class="inner">
+    <slot />
+  </div>
+</button>
 
 <style lang="scss" scoped>
   button {
-    align-items: center;
-    background-color: #fff;
     border: none;
-    border-bottom: 0.25rem solid #86a789;
     border-radius: 0.5rem;
     cursor: pointer;
-    display: flex;
+    display: block;
     font-size: inherit;
-    gap: 0.25rem;
     margin-bottom: 1rem;
-    padding: 1rem 1.5rem;
+    padding: 0;
+    position: relative;
+    -webkit-tap-highlight-color: transparent;
     transition: transform 0.15s ease-in-out;
+    user-select: none;
+
+    &:hover,
+    &:focus {
+      .inner {
+        transform: translate3d(-0.175rem, -0.35rem, 0);
+        transition-duration: 0.15s;
+      }
+    }
 
     &:active {
-      border-bottom: none;
-      margin-top: 0.25rem;
-      transform: translateY(0.25rem);
+      .inner {
+        transform: translate3d(-0.025rem, -0.1rem, 0);
+        transition-duration: 0.1s;
+      }
     }
+
+    &::before {
+      background-color: #86a789;
+      border-radius: inherit;
+      content: "";
+      height: 100%;
+      left: 0;
+      position: absolute;
+      top: 0;
+      width: 100%;
+    }
+  }
+
+  .inner {
+    align-items: center;
+    background-color: #fff;
+    border-radius: inherit;
+    display: flex;
+    gap: 0.25rem;
+    padding: 1rem 1.5rem;
+    position: relative;
+    transform: translate3d(-0.125rem, -0.25rem, 0);
+    transition: transform 0.25s ease-in-out;
   }
 </style>
